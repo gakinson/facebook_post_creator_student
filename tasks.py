d11035517186cd18973a7be2c1ae9e7325377484
Geoffrey Kinson <gakinson@productwind.com>
2024-08-18 19:08:27 -0700

    save changes

--- tasks.py
+++ tasks.py
@@ -5,7 +5,7 @@ from agents import social_media_content_creator, social_media_researcher, tag_de
 
 scrape_article = Task(
     description=("Scrape the {url}."
-    "Get the relevant information and content from the article included."),
+                 "Get the relevant information and content from the article included."),
     expected_output= "A one paragraph summary about the article in the {url}.",
     tools = [scrape_tool],
     agent = social_media_researcher
@@ -17,8 +17,7 @@ write_blurb = Task(
     expected_output= "A one sentence blurb that can be posted to Facebook from the article at {url}.",
     tools = [scrape_tool],
     async_execution= False,
-    agent = social_media_content_creator,
-    output_file = 'facebook_blurb.md'
+    agent = social_media_content_creator
 )
 
 
